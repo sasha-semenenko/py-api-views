a32e930e4a234317e8815387919e1f6a9d81ab31
Sasha Semenenko <semenenko233249@gmail.com>
2023-10-09 22:26:41 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, CinemaHall, Actor
 
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
+    title = serializers.CharField(max_length=64)
+    description = serializers.CharField(max_length=64)
     duration = serializers.IntegerField()
 
     def create(self, validated_data):
@@ -22,3 +22,33 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=64)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row)
+        instance.save()
+        return instance
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
